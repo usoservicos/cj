9e66b72401afe46872572521c0f10204bdd27ea3
usoservicos <160978674+usoservicos@users.noreply.github.com>
2024-02-26 17:27:30 -0300

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -17,8 +17,7 @@ SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [cj-2vju.onrender.com, localhost]
 
 # Application definition
 
